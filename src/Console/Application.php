cbee1bbb4c94b90babe0ae0c5c66f6eb712c7146
Christer Edvartsen <cogo@starzinger.net>
2026-02-26 07:39:53 +0100

    feat: add CLI application and command

---
+++ src/Console/Application.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace ImboReleaser\Console;
+
+use ImboReleaser\Command\Release;
+use ImboReleaser\GitHub\Client;
+use Symfony\Component\Console\Application as BaseApplication;
+
+class Application extends BaseApplication
+{
+    public function __construct(Client $gitHubClient)
+    {
+        parent::__construct('Imbo releaser');
+        $this->addCommand(new Release($gitHubClient));
+    }
+}
