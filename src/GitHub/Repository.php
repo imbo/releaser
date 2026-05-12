7815857af06a3682d240c6f11f98cedd22fdc557
Christer Edvartsen <cogo@starzinger.net>
2026-05-12 09:55:49 +0200

    feat: add factory method with validation

--- src/GitHub/Repository.php
+++ src/GitHub/Repository.php
@@ -2,8 +2,10 @@
 
 namespace ImboReleaser\GitHub;
 
+use InvalidArgumentException;
 use Stringable;
 
+use function count;
 use function sprintf;
 
 final class Repository implements Stringable
@@ -17,5 +19,15 @@ final class Repository implements Stringable
     public function __toString(): string
     {
         return sprintf('%s/%s', $this->owner, $this->repo);
+    }
+
+    public static function fromString(string $repository): self
+    {
+        $parts = explode('/', $repository);
+        if (2 !== count($parts)) {
+            throw new InvalidArgumentException(sprintf('Invalid repository string "%s", expected format "owner/repo"', $repository));
+        }
+
+        return new self($parts[0], $parts[1]);
     }
 }
--- tests/GitHub/RepositoryTest.php
+++ tests/GitHub/RepositoryTest.php
@@ -2,6 +2,7 @@
 
 namespace ImboReleaser\GitHub;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
@@ -12,5 +13,18 @@ class RepositoryTest extends TestCase
     {
         $repository = new Repository('imbo', 'releaser');
         $this->assertSame('imbo/releaser', (string) $repository);
+    }
+
+    public function testFromString(): void
+    {
+        $repository = Repository::fromString('imbo/releaser');
+        $this->assertSame('imbo', $repository->owner);
+        $this->assertSame('releaser', $repository->repo);
+    }
+
+    public function testFromStringWithInvalidString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Repository::fromString('invalid-repo-string');
     }
 }
