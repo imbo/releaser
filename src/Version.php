d888379474157985b876eff10f3cb67726d0ddc4
Christer Edvartsen <cogo@starzinger.net>
2026-05-29 08:31:39 +0200

    feat: add Version class with test

---
+++ src/Version.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace ImboReleaser;
+
+use InvalidArgumentException;
+use Stringable;
+
+use function sprintf;
+
+final class Version implements Stringable
+{
+    public function __construct(private ?string $prefix = null, private int $major = 0, private int $minor = 0, private int $patch = 0)
+    {
+    }
+
+    public function __toString(): string
+    {
+        return ($this->prefix ?? '').$this->major.'.'.$this->minor.'.'.$this->patch;
+    }
+
+    public function incrementMajor(): self
+    {
+        return new self($this->prefix, $this->major + 1, 0, 0);
+    }
+
+    public function incrementMinor(): self
+    {
+        return new self($this->prefix, $this->major, $this->minor + 1, 0);
+    }
+
+    public function incrementPatch(): self
+    {
+        return new self($this->prefix, $this->major, $this->minor, $this->patch + 1);
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public static function fromString(string $version): self
+    {
+        if (!preg_match('/^(?P<prefix>.*?)(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)$/', $version, $matches)) {
+            throw new InvalidArgumentException(sprintf('Invalid version string: "%s"', $version));
+        }
+
+        return new self(
+            '' !== $matches['prefix'] ? $matches['prefix'] : null,
+            (int) $matches['major'],
+            (int) $matches['minor'],
+            (int) $matches['patch'],
+        );
+    }
+}
