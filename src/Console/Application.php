084b068678bf64f5ebec8651babd7e1154d9a3c6
Christer Edvartsen <cogo@starzinger.net>
2026-06-04 22:40:50 +0200

    feat: take ownership of the list command name
    Rename the `list` command to `commands`, and set `commands` as the
    default command in the application. This way the application still works
    as expected when no command is given, and we can use the `list` command
    for something else.

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -5,6 +5,8 @@ namespace ImboReleaser\Console;
 use ImboReleaser\Command;
 use ImboReleaser\GitHub\Client;
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Command\ListCommand as SymfonyListCommand;
 
 class Application extends BaseApplication
 {
@@ -12,7 +14,25 @@ class Application extends BaseApplication
     public function __construct(Client $gitHubClient)
     {
         parent::__construct('Imbo releaser');
+        $this->setDefaultCommand('commands');
         $this->addCommand(new Command\CreateRelease($gitHubClient));
         $this->addCommand(new Command\ListReleases($gitHubClient));
+    }
+
+    /**
+     * @return array<SymfonyCommand>
+     */
+    protected function getDefaultCommands(): array
+    {
+        $defaults = parent::getDefaultCommands();
+
+        foreach ($defaults as $command) {
+            if ($command instanceof SymfonyListCommand) {
+                $command->setName('commands');
+                break;
+            }
+        }
+
+        return $defaults;
     }
 }
